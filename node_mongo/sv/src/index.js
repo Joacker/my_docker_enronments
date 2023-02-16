78aaf557c205781177d2620ce85defeb5ea2f55b
joacker <jfernandezvizcarra@gmail.com>
2023-02-16 12:29:49 -0300

    new commit

--- node_mongo/sv/src/index.js
+++ node_mongo/sv/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const _connect = require('./db/connect');
 const bodyParser = require('body-parser');
-
+const userRouter = require('./routes/userRouter');    
 
 require('dotenv').config();
 // mongo connection
@@ -12,7 +12,9 @@ const app = express()
 
 app.use(morgan('dev'));
 
+// Routes
+app.use('/account', userRouter);
+
 app.listen(process.env.PORT, () => {
-    console.log("hola")
     console.log(`Server is running on port ${process.env.PORT}`);
 });
--- node_mongo/sv/src/models/users.js
+++ node_mongo/sv/src/models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
+const { isValidEmail } = require('../helpers');
 
 const userSchema = new mongoose.Schema({
     email: {
