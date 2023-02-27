f75617bfa713f6c9ac3183affa23154d76ea571f
joacker <jfernandezvizcarra@gmail.com>
2023-02-27 12:51:15 -0300

    new commit

--- node_mongo/sv/src/models/todo.js
+++ node_mongo/sv/src/models/todo.js
@@ -23,17 +23,11 @@ mongoose.model('todo', TodoSchema, 'todos');
 
 // methods
 function create(todoInfo, user) {
-    //print the todoInfo object key value pairs
-    // for (const [key, value] of Object.entries(todoInfo)) {
-    //     console.log(`${key}: ${value}`);
-    //     todoInfo[key] = value;
-    //     console.log(todoInfo[key])
-    // }    
-    console.log(todoInfo.title)
-
+    console.log(user)
     if (!todoInfo.title) throw new Error('Title is required');
 
     todoInfo.userId = user._id;
+    
     //console.log(todoInfo.userId)
     const todo = new this(todoInfo);
     return todo.save(); 
